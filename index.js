f8e7b73446162edc12ce9d3848cbfbafee878070
napsev1 <matasnematas@gmail.com>
2023-08-07 17:48:25 +0300

    made button 'MORE...' work + styling

---
+++ index.js
@@ -0,0 +1,8 @@
+//atidaro div kai pamygi migtuka
+function showDiv() {
+    document.getElementById('expandingdiv').style.display = "block";
+ }
+ // as esu genijus no cap
+ function closeDiv(){ 
+    document.getElementById("expandingdiv").style.display = "none"
+    } 
