15fa36d67848d0854951464c5bc18677dcd686a4
napsev1 <matasnematas@gmail.com>
2023-08-07 20:32:35 +0300

    diary real

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ function showDiv() {
  function closeDiv(){
    document.getElementById("expandingdiv").style.display = "none";
 } 
+
+// darau tai ant prikolo ismokt labiau js
+// ir kad tu suprastum si js coda ir pasymokitum is jo
+// newElement funkcija (ja callina html code)
+function newElement() {
+   // gaunam text ivarom ji i variable "value"
+   var value = document.getElementById("myInput").value;
+     // sukuriam nauja listo elementa 
+     var li = document.createElement("li");
+     // Set the text content to the value
+     li.textContent = value;
+     // perkeliam i item i lista
+     myUL.appendChild(li);
+     // paclearinam kai pamygam mygtuka
+     myInput.value = "";
+   }
+ 
+
+
+ 
